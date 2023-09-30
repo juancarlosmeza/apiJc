7829d4f7c6b439eab42884800b2c09f33215618f
juan <102879013+juancarlosmeza@users.noreply.github.com>
2023-09-30 10:57:46 -0300

    Update conexion.js

--- conf/conexion.js
+++ conf/conexion.js
@@ -6,18 +6,19 @@ const conexion=mysql.createConnection({
    //password:"cFjV2JBe38",
    //port:"3306",
    //database:"sql10584711"
-  // host:"mysql-101398-0.cloudclusters.net",
-   // user:"juanca",
-   // password:"Coop1234+1",
-   // port:"10143",
-   // database:"db_basico"
+   
+    host:"mysql-101398-0.cloudclusters.net",
+    user:"juanca",
+    password:"Coop1234+1",
+    port:"10143",
+    database:"db_basico"
 
-    //host: "db4free.net/phpMyAdmin",
-      host:"db4free.net", 
-   user:"juanca",
-    password:"484000co",
-    port:"3306",
-    database:"juancabd"
+    
+    //host:"db4free.net", 
+    //user:"juanca",
+    //password:"484000co",
+    //port:"3306",
+    //database:"juancabd"
    
     
 });
